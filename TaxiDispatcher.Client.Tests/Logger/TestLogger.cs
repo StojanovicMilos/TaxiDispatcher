ccddad999ef09d3f79b3d1da8f9620642c0df268
StojanovicMilos <s.milos@sbb.rs>
2018-12-25 20:08:25 +0100

    Write overall test to confirm functionality

---
+++ TaxiDispatcher.Client.Tests/Logger/TestLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaxiDispatcher.Client.Tests.Logger
+{
+    public class TestLogger : ILogger
+    {
+        public string AllMessages { get; set; }
+
+        public void WriteLine(string message)
+        {
+            AllMessages += message + Environment.NewLine;
+        }
+    }
+}
---
+++ TaxiDispatcher.Client.Tests/OverallFunctionalityTests/OverallFunctionalityTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TaxiDispatcher.Client.Tests.Logger;
+
+namespace TaxiDispatcher.Client.Tests.OverallFunctionalityTests
+{
+    [TestClass]
+    public class OverallFunctionalityTests
+    {
+        [TestMethod]
+        public void OverallFunctionalityTest()
+        {
+            //arrange
+            TestLogger logger = new TestLogger();
+            string expectedMessages = "Ordering ride from 5 to 0..." + Environment.NewLine + Environment.NewLine + "Ordering ride from 0 to 12..."
+                + Environment.NewLine + Environment.NewLine + "Ordering ride from 5 to 0..." + Environment.NewLine + Environment.NewLine + "Ordering ride from 35 to 12..."
+                + Environment.NewLine + "There are no available taxi vehicles!" + Environment.NewLine + Environment.NewLine + "Driver with ID = 2 earned today:"
+                + Environment.NewLine + "Price: 100" + Environment.NewLine + "Price: 240" + Environment.NewLine + "Total: 340" + Environment.NewLine;
+
+            //act
+            Program.RunTaxiDispatcher(logger);
+
+            //assert
+            Assert.AreEqual(expectedMessages, logger.AllMessages);
+        }
+    }
+}
