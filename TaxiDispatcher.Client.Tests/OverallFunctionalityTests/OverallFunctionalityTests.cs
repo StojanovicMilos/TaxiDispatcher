a44e067fe9e2e877db3cb8f162e811fc9659d1e9
StojanovicMilos <s.milos@sbb.rs>
2019-01-13 08:28:59 +0100

    fix wrong commit

--- TaxiDispatcher.Client.Tests/OverallFunctionalityTests/OverallFunctionalityTests.cs
+++ TaxiDispatcher.Client.Tests/OverallFunctionalityTests/OverallFunctionalityTests.cs
@@ -16,12 +16,12 @@ namespace TaxiDispatcher.Tests.OverallFunctionalityTests
             TaxiDispatcherClient taxiDispatcherClient = new TaxiDispatcherClient(logger, testDatabase);
             string expectedMessages = "Ordering ride from 5 to 0..." + Environment.NewLine +
                                       "Ride ordered, price: 100" + Environment.NewLine +
-                                      "Ride accepted, waiting for driver: Nenad"
+                                      "Ride accepted, waiting for driver: Nenad" + Environment.NewLine
                                       + Environment.NewLine + "Ordering ride from 0 to 12..."
                                       + Environment.NewLine + "Ride ordered, price: 240" + Environment.NewLine +
-                                      "Ride accepted, waiting for driver: Nenad" + Environment.NewLine +
+                                      "Ride accepted, waiting for driver: Nenad" + Environment.NewLine + Environment.NewLine + 
                                       "Ordering ride from 5 to 0..." + Environment.NewLine + "Ride ordered, price: 75" +
-                                      Environment.NewLine + "Ride accepted, waiting for driver: Dragan"
+                                      Environment.NewLine + "Ride accepted, waiting for driver: Dragan" + Environment.NewLine
                                       + Environment.NewLine + "Ordering ride from 35 to 12..." + Environment.NewLine +
                                       "There are no available taxi vehicles!" + Environment.NewLine
                                       + Environment.NewLine + "Driver with ID = 2 earned today:" + Environment.NewLine +
