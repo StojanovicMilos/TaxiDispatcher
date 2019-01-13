81e66e41b58ed099438f488e9fb8f39552ba5633
StojanovicMilos <s.milos@sbb.rs>
2019-01-13 08:25:29 +0100

    change initialization of array, add string interpolation, fix typo in client and refactor overall test.

--- TaxiDispatcher.Client.Tests/OverallFunctionalityTests/OverallFunctionalityTests.cs
+++ TaxiDispatcher.Client.Tests/OverallFunctionalityTests/OverallFunctionalityTests.cs
@@ -14,11 +14,19 @@ namespace TaxiDispatcher.Tests.OverallFunctionalityTests
             TestLogger logger = new TestLogger();
             TestDatabase testDatabase = new TestDatabase();
             TaxiDispatcherClient taxiDispatcherClient = new TaxiDispatcherClient(logger, testDatabase);
-            string expectedMessages = "Ordering ride from 5 to 0..." + Environment.NewLine + "Ride ordered, price: 100" + Environment.NewLine + "Ordering ride from 0 to 12..."
-                + Environment.NewLine + "Ride ordered, price: 240" +  Environment.NewLine + "Ordering ride from 5 to 0..." + Environment.NewLine + "Ride ordered, price: 75"
-                + Environment.NewLine + "Ordering ride from 35 to 12..." + Environment.NewLine + "There are no available taxi vehicles!" + Environment.NewLine 
-                + Environment.NewLine + "Driver with ID = 2 earned today:" + Environment.NewLine + "Price: 100" + Environment.NewLine + "Price: 240" + Environment.NewLine 
-                + "Total: 340" + Environment.NewLine;
+            string expectedMessages = "Ordering ride from 5 to 0..." + Environment.NewLine +
+                                      "Ride ordered, price: 100" + Environment.NewLine +
+                                      "Ride accepted, waiting for driver: Nenad"
+                                      + Environment.NewLine + "Ordering ride from 0 to 12..."
+                                      + Environment.NewLine + "Ride ordered, price: 240" + Environment.NewLine +
+                                      "Ride accepted, waiting for driver: Nenad" + Environment.NewLine +
+                                      "Ordering ride from 5 to 0..." + Environment.NewLine + "Ride ordered, price: 75" +
+                                      Environment.NewLine + "Ride accepted, waiting for driver: Dragan"
+                                      + Environment.NewLine + "Ordering ride from 35 to 12..." + Environment.NewLine +
+                                      "There are no available taxi vehicles!" + Environment.NewLine
+                                      + Environment.NewLine + "Driver with ID = 2 earned today:" + Environment.NewLine +
+                                      "Price: 100" + Environment.NewLine + "Price: 240" + Environment.NewLine
+                                      + "Total: 340" + Environment.NewLine;
 
             //act
             taxiDispatcherClient.Run();
--- TaxiDispatcher.Client/TaxiDispatcherClient.cs
+++ TaxiDispatcher.Client/TaxiDispatcherClient.cs
@@ -14,8 +14,7 @@ namespace TaxiDispatcher.Client
         private readonly ILogger _logger;
         private readonly TaxiContext _taxiContext;
         private readonly Scheduler _scheduler;
-        private readonly RideOrder[] _rideOrders = new RideOrder[]
-            {
+        private readonly RideOrder[] _rideOrders = {
                 new RideOrder { StartLocation = new Location(5), DestinationLocation = new Location(0), RideType = RideType.City, RideDateTime = new DateTime(2018, 1, 1, 23, 0, 0) },
                 new RideOrder { StartLocation = new Location(0), DestinationLocation = new Location(12), RideType = RideType.InterCity, RideDateTime = new DateTime(2018, 1, 1, 9, 0, 0) },
                 new RideOrder { StartLocation = new Location(5), DestinationLocation = new Location(0), RideType = RideType.City, RideDateTime = new DateTime(2018, 1, 1, 11, 0, 0) },
@@ -60,18 +59,18 @@ namespace TaxiDispatcher.Client
 
         private void PerformRide(RideOrder rideOrder)
         {
-            _logger.WriteLine(string.Format("Ordering ride from {0} to {1}...", rideOrder.StartLocation, rideOrder.DestinationLocation));
+            _logger.WriteLine($"Ordering ride from {rideOrder.StartLocation} to {rideOrder.DestinationLocation}...");
             var ride = _scheduler.OrderRide(rideOrder);
-            _logger.WriteLine("Ride ordered, price: " + ride.Price.ToString());
+            _logger.WriteLine("Ride ordered, price: " + ride.Price);
             _scheduler.AcceptRide(ride);
-            Console.WriteLine("Ride accepted, waiting for driver: " + ride.RideTaxi.TaxiDriverName + Environment.NewLine);
+            _logger.WriteLine("Ride accepted, waiting for driver: " + ride.RideTaxi.TaxiDriverName + Environment.NewLine);
         }
 
         private TaxiDTO GetTaxiWithEarningsById(int id) => _taxiContext.GetTaxiWithEarningsById(id);
 
         private void LogTaxiWithEarnings(TaxiDTO taxiWithEarnings)
         {
-            _logger.WriteLine(string.Format("Driver with ID = {0} earned today:", taxiWithEarnings.TaxiDriverId));
+            _logger.WriteLine($"Driver with ID = {taxiWithEarnings.TaxiDriverId} earned today:");
             foreach (var ride in taxiWithEarnings.Rides)
             {
                 _logger.WriteLine("Price: " + ride.Price);
